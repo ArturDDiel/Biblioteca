32e8baf752bc2a6807d764185ec9c5420444fc56
Artur Daniel Diel <arturdanieldiel@gmail.com>
2023-07-29 18:02:47 -0300

    alteraçoes

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
 }
 
-// function pegaArquivo(caminhoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises.readFile(caminhoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados.length !== 0 ? resultados : "Não há links no arquivo";
+}
 
 async function pegaArquivo(caminhoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoArquivo, encoding);
-        console.log(chalk.green(texto));
+        return extraiLinks(texto);
     } catch (erro) {
         trataErro(erro)
     }
 }
 
-pegaArquivo('./arquivos/texto.md')
+export default pegaArquivo;
