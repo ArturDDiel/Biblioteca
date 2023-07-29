5cdaf35792d90851c83a1d240f578e77b85df456
Artur Daniel Diel <arturdanieldiel@gmail.com>
2023-07-29 01:57:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+import chalk from "chalk";
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
+}
+
+// function pegaArquivo(caminhoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(caminhoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+// }
+
+async function pegaArquivo(caminhoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+pegaArquivo('./arquivos/texto.md')
